42a75af376124e5789c416b3f8a36aa8093be567
Derek-X-Wang <derekxinzhewang@gmail.com>
2015-08-12 02:55:20 +0800

    Database: init the manager class

--- app/src/main/java/com/intbridge/projects/gaucholife/PGDatabaseManager.java
+++ app/src/main/java/com/intbridge/projects/gaucholife/PGDatabaseManager.java
@@ -1,8 +1,24 @@
 package com.intbridge.projects.gaucholife;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 /**
  * Created by Derek on 8/10/2015.
  */
 public class PGDatabaseManager {
-    
+    public void getUCSBCommonsDataFromHTML(String year,String month,String day){
+        Document doc = null;
+        try {
+            String url = "https://appl.housing.ucsb.edu/" + "menu?day=" + year + "-" + month + "-" + day + "&meal=";
+            doc = Jsoup.connect(url).get();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
 }
