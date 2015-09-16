7671906eaf2c5335aa49e97c3d9d29c002af5566
Derek-X-Wang <derekxinzhewang@gmail.com>
2015-09-16 23:35:13 +0800

    move splashscreen out of stack

--- app/src/main/java/com/intbridge/projects/gaucholife/PGSplashScreen.java
+++ app/src/main/java/com/intbridge/projects/gaucholife/PGSplashScreen.java
@@ -2,7 +2,6 @@ package com.intbridge.projects.gaucholife;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -43,53 +42,10 @@ public class PGSplashScreen extends Activity {
                     i.putExtra("DATASOURCE", object.getBoolean("DataSource"));
                     i.putExtra("CLEANLOCAL", object.getBoolean("CleanLocal"));
                     startActivity(i);
+                    finish();
                 }
             }
         });
     }
 
-    /**
-     * Async Task to make http call
-     */
-    private class PrefetchData extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // before making http calls
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            /*
-             * Will make http call here This call will download required data
-             * before launching the app
-             * example:
-             * 1. Downloading and storing in SQLite
-             * 2. Downloading images
-             * 3. Fetching and parsing the xml / json
-             * 4. Sending device information to server
-             * 5. etc.,
-             */
-
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            // After completing http call
-            // will close this activity and lauch main activity
-            Intent i = new Intent(PGSplashScreen.this, MainActivity.class);
-            i.putExtra("now_playing", now_playing);
-            i.putExtra("earned", earned);
-            startActivity(i);
-
-            // close this activity
-            finish();
-        }
-
-    }
 }
