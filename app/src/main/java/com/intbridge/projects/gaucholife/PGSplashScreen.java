de134e7b9c1c9a666692aa264c601782ec591e0f
Derek Wang <derekxinzhewang@gmail.com>
2017-01-09 15:46:28 -0800

    Refactor -> SplashScreen apply RxJava

--- app/src/main/java/com/intbridge/projects/gaucholife/PGSplashScreen.java
+++ app/src/main/java/com/intbridge/projects/gaucholife/PGSplashScreen.java
@@ -19,12 +19,15 @@ import com.pixplicity.easyprefs.library.Prefs;
 
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 import rx.Observable;
 import rx.Observer;
 import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  *
@@ -34,9 +37,10 @@ public class PGSplashScreen extends Activity {
     // Splash screen timer
     public static String DATA_SOURCE = "DATASOURCE";
     public static String CLEAN_LOCAL = "CLEANLOCAL";
-    private static int SPLASH_TIME_OUT = 6000;
+
+    private static int SPLASH_TIME_OUT = 2000;
     private SweetAlertDialog dialog;
-    private boolean FORCE_ENTER = true;
+    private boolean DATA_RECEIVED = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,76 +52,60 @@ public class PGSplashScreen extends Activity {
         Fader.runAlphaAnimation(this, R.id.imgLogo);
         final ImageView logo = (ImageView)findViewById(R.id.imgLogo);
         if(isNetworkConnected()){
-
-//            Observable<ParseObject> request = Observable.fromCallable(new Callable<ParseObject>() {
-//                @Override
-//                public ParseObject call() throws Exception {
-//                    return ParseQuery.getQuery("ControlPanel").getFirst();
-//                }
-//            });
-//            Observer<ParseObject> observer = new Observer<ParseObject>() {
-//                @Override
-//                public void onCompleted() {
-//
-//                }
-//
-//                @Override
-//                public void onError(Throwable e) {
-//
-//                }
-//
-//                @Override
-//                public void onNext(ParseObject parseObject) {
-//                    if (parseObject == null) {
-//
-//                    } else {
-//                        logo.clearAnimation();
-//                        Prefs.putBoolean(DATA_SOURCE, parseObject.getBoolean("DataSource"));
-//                        Prefs.putBoolean(CLEAN_LOCAL, parseObject.getBoolean("CleanLocal"));
-//                    }
-//                }
-//            };
-
-            ParseQuery query = ParseQuery.getQuery("ControlPanel");
-            query.whereEqualTo("OS","Android");
-            query.getFirstInBackground(new GetCallback<ParseObject>() {
-                public void done(ParseObject object, ParseException e) {
-
-                    if (object == null) {
-                        Log.d("Parse Query", "The getFirst request failed.");
-                    } else {
-                        FORCE_ENTER = false;
-                        if(dialog.isShowing()){
-                            dialog.dismiss();
-                        }
-                        // get the panel
-                        logo.clearAnimation();
-                        Intent i = new Intent(PGSplashScreen.this, MainActivity.class);
-                        Prefs.putBoolean(DATA_SOURCE, object.getBoolean("DataSource"));
-                        Prefs.putBoolean(CLEAN_LOCAL, object.getBoolean("CleanLocal"));
-                        startActivity(i);
-                        finish();
-                    }
-                }
-            });
-
-            new Handler().postDelayed(new Runnable() {
+            Observable.fromCallable(new Callable<ParseObject>() {
                 @Override
-                public void run() {
-                    if(FORCE_ENTER) {
-                        dialog.setCancelClickListener(new SweetAlertDialog.OnSweetClickListener(){
-                            @Override
-                            public void onClick(SweetAlertDialog sweetAlertDialog) {
-                                sweetAlertDialog.dismiss();
-                            }
-                        });
-                        dialog.show();
-                    }
+                public ParseObject call() throws Exception {
+                    return ParseQuery
+                            .getQuery("ControlPanel")
+                            .whereEqualTo("OS","Android")
+                            .getFirst();
                 }
-            }, SPLASH_TIME_OUT);
-        }else{
-            dialog.show();
-        }
+            })
+                    .mergeWith(Observable.timer(SPLASH_TIME_OUT, TimeUnit.MILLISECONDS)
+                            .map(new Func1<Long, ParseObject>() {
+                                @Override
+                                public ParseObject call(Long aLong) {
+                                    return null;
+                                }
+                            }))
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<ParseObject>() {
+                        @Override
+                        public void onCompleted() {
+                            Log.d("Stream onCompleted: ", "stream completed");
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+
+                        }
+
+                        @Override
+                        public void onNext(ParseObject parseObject) {
+                            // DATA_RECEIVED prevent open dialog after data is received
+                            if (parseObject == null) {
+                                if(!DATA_RECEIVED) {
+                                    Log.e("Parse Request: ", "request timeout, show dialog");
+                                    dialog.setTitleText("Internet Congestion!");
+                                    dialog.show();
+                                }
+                            } else {
+                                if(dialog.isShowing()){
+                                    Log.e("Parse Request: ", "dialog is showing");
+                                    dialog.dismiss();
+                                }
+                                DATA_RECEIVED = true;
+                                Log.e("Parse Request: ", "object received");
+                                logo.clearAnimation();
+                                Prefs.putBoolean(DATA_SOURCE, parseObject.getBoolean("DataSource"));
+                                Prefs.putBoolean(CLEAN_LOCAL, parseObject.getBoolean("CleanLocal"));
+                                startActivity(new Intent(PGSplashScreen.this, MainActivity.class));
+                                finish();
+                            }
+                        }
+                    });
+        } else { dialog.show();}
 
     }
 
